8bf4dcd38231b674bc5c6dead9a34f6329831b2c
Phillip_Chan <phillip.chan@liferay.COM>
2018-01-24 13:05:13 -0800

    Working generator

---
+++ generators/app/index.js
@@ -0,0 +1,56 @@
+'use strict';
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = Generator.extend({
+  prompting: function() {
+    // Have Yeoman greet the user.
+    this.log(yosay(`Let's generatote a ${chalk.red('metaljs component')}!`));
+
+    var prompts = [
+      {
+        name: 'componentName',
+        message: `What is your component's name?`,
+        default: this.componentName,
+        required: true
+      }
+    ];
+
+    return this.prompt(prompts).then(
+      function(props) {
+        // To access props later use this.props.someAnswer;
+        this.props = props;
+      }.bind(this)
+    );
+  },
+
+  writing: function() {
+    this.fs.copyTpl(
+      this.templatePath('Component.js'),
+      this.destinationPath(
+        `${this.props.componentName}/${this.props.componentName}.js`
+      ),
+      {
+        componentName: this.props.componentName
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('index.js'),
+      this.destinationPath(`${this.props.componentName}/index.js`),
+      {
+        componentName: this.props.componentName
+      }
+    );
+
+    // this.template(
+    //   `${this.templatePath}/Component.js`,
+    //   this.componentName + '.js'
+    // );
+  },
+
+  install: function() {
+    this.installDependencies();
+  }
+});
---
+++ generators/app/templates/Component.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import JSXComponent from 'metal-jsx';
+
+class <%= componentName %> extends JSXComponent {
+	render() {
+		return (
+			<div>
+				<%= componentName %> component Works!
+			</div>
+		);
+	}
+}
+
+export default <%= componentName %>;
